e3290e50c04a963269c6f3710f733fd21dbc015b
9chris5 <kheciek@gmail.com>
2017-09-30 22:09:18 +0200

    Update

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,17 @@
-function phone(brand, price, color) {
+function Phone(brand, price, color) {
     this.brand = brand;
     this.color = color;
     this.price = price;
 }
-phone.prototype.printInfo = function() {
+Phone.prototype.printInfo = function() {
     console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
 }
-phone.prototype.applyDiscount = function (discount) {
+Phone.prototype.applyDiscount = function (discount) {
     this.price -= discount;
 }
-var iPhone6S = new phone("Apple", 2500, "gold"),
-    samsungGalaxyS6 = new phone("Samsung", 2200, "silver"),
-    onePlusOne = new phone("OnePlus", 2000, "black"),
+var iPhone6S = new Phone("Apple", 2500, "gold"),
+    samsungGalaxyS6 = new Phone("Samsung", 2200, "silver"),
+    onePlusOne = new Phone("OnePlus", 2000, "black"),
     discountAmount = 250;
 
 iPhone6S.printInfo();
