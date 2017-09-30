04723a57b45e1be4f13cc4a287e26ab8f38661f6
Krzysiu <kheciek@gmail.com>
2017-09-30 21:46:53 +0200

    applyDiscount function added

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,25 @@
-function Phone(brand, price, color) {
+function phone(brand, price, color) {
     this.brand = brand;
     this.color = color;
     this.price = price;
 }
-Phone.prototype.printInfo = function() {
+phone.prototype.printInfo = function() {
     console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
 }
-
-var iPhone6S = new Phone("Apple", 2500, "gold"),
-    SamsungGalaxyS6 = new Phone("Samsung", 2200, "silver"),
-    OnePlusOne = new Phone("OnePlus", 2000, "black");
+phone.prototype.applyDiscount = function (discount) {
+    this.price -= discount;
+}
+var iPhone6S = new phone("Apple", 2500, "gold"),
+    samsungGalaxyS6 = new phone("Samsung", 2200, "silver"),
+    onePlusOne = new phone("OnePlus", 2000, "black"),
+    discountAmount = 250;
 
 iPhone6S.printInfo();
-SamsungGalaxyS6.printInfo();
-OnePlusOne.printInfo();
+samsungGalaxyS6.printInfo();
+onePlusOne.printInfo();
+iPhone6S.applyDiscount(discountAmount);
+samsungGalaxyS6.applyDiscount(discountAmount);
+onePlusOne.applyDiscount(discountAmount);
+iPhone6S.printInfo();
+samsungGalaxyS6.printInfo();
+onePlusOne.printInfo();
